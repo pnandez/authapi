6b61f665e753b5260f408e6b767d9b26d1660453
Pablo Hernández <Pablo@Pablos-MacBook-Pro.local>
2020-08-20 11:47:11 +0200

    User creation and searching improved. Class for db queries created

--- api.py
+++ api.py
@@ -1,33 +1,31 @@
 from flask import Flask, request, abort, redirect, render_template, jsonify, make_response
 import sqlite3 as sql
-from setup import User
+from setup import User, DB
 from flask_jwt_extended import JWTManager
 
-db_name = "users"
-db_fields = ["email", "password_hash", "admin"]
 app = Flask(__name__)
 app.config.from_envvar('ENV_FILE_LOCATION')
+
+db_table = "users"
+db_fields = ["email", "password_hash", "admin"]
+database = "database.db"
 jwt = JWTManager(app)
 
-msg = ''
-
-db_connection = sql.connect("database.db")
-db_cursor = db_connection.cursor()
-if not sql.connect("databas.db"):
+db = DB(database)
+if not sql.connect(database):
   os.system("createdb.py")
 
 
 #Page to display a table with al the users and if they are admin or not
 @app.route('/userinfo', methods=["GET"])
 def user_display_admin():
-  with sql.connect("database.db") as con:
-   cur = con.cursor()
-   cur.execute("SELECT * from users")
-   records = cur.fetchall()
-   users = []
-   for record in records:
-     users.append(dict(username=record[0], admin=record[-1]))
+  records = db.get_all("users")
+  users = []
+  for record in records:
+    users.append(dict(username=record[0], admin=record[-1]))
   return render_template("user_info.html", users=users)
+
+  #WORKS
 
 
 
@@ -41,18 +39,13 @@ def user_form():
     user = User(request.form["username"], request.form["passwd"])
     if request.form.getlist("admin"):
       user.admin = "YES"
-    with sql.connect("database.db") as con:
-      cur = con.cursor()
-      cur.execute("SELECT EXISTS(SELECT * FROM users WHERE email LIKE '%s')" % user.user)
-      users_list = cur.fetchone()
-      if users_list is not None:
-        msg = "Usuario existente."
+    if db.exists(db_table, db_fields[0], user.user):
+      return redirect("userinfo")
+    else:
+      if db.insert(db_table, db_fields, user):
         return redirect("userinfo")
       else:
-        cur.execute("INSERT INTO %s (%s, %s, %s) VALUES ('%s', '%s', '%s')" % (db_name, db_fields[0], db_fields[1], db_fields[2], user.user, user.passwd, user.admin))
-        con.commit()
-        msg = "Usuario creado correctamente."
-        return redirect("/userinfo")
+        return jsonify({"Message" : "User not created"})
   else:
     return render_template("new_user.html")
 
@@ -61,25 +54,23 @@ def user_form():
 #Get information of a certain user
 @app.route('/user/<username>', methods=["GET"])
 def get_users(username):
-  with sql.connect("database.db") as con:
-    cur = con.cursor()
-    cur.execute("SELECT * from users WHERE email like '%s'" % username)
-    records = cur.fetchall()
-    if records:
-      user = User(records[0][0], records[0][1], records[0][2])
-      return user.user + user.passwd + user.admin
-    else:
-      return "User doesn´t exist"
+  records = db.search( table="users", column="email", data=username)
+  if records:
+    user = User(records[0][0], records[0][1], records[0][2])
+    return user.user + "\t " + user.passwd + "\t" + user.admin
+  else:
+    return "User doesn´t exist"
 
 
 @app.route('/user/<username>', methods=["DELETE"])
 def del_users(username):
 #TODO implement search first for user(error handling)
-  with sql.connect("database.db") as con:
-    cur = con.cursor()
-    cur.execute("DELETE from users WHERE email LIKE '%s'" % username)
-    
-    return redirect("/user")
+  if db.delete(db_table, db_fields[0], username):
+    return jsonify({"Message" : "User deleted"})
+  else:
+    return jsonify({"Message": "User not deleted"})
+
+  return redirect("/user")
 
 
 @app.route("/login")
@@ -94,7 +85,8 @@ def login():
     if records: #If the user exists
       user = User(records[0][0], records[0][1], records[0][2])
       if user.verify_passwd(password=password_given):
-        return "YAYY"
+        user.get_token()
+        return 
     else:
       #TODO implement error handling if user doesn´t exist
       return ''
--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from passlib.apps import custom_app_context as pwd_context
 from flask_jwt_extended import create_access_token
+import sqlite3 as sql
+
 
 class User:
   def __init__(self, user, password, admin="NO"):
@@ -14,5 +16,45 @@ class User:
   def verify_passwd(self, password):
     return pwd_context.verify(password, self.passwd)
 
-  
 
+class DB:
+  def __init__(self, database):
+    self.database = database
+
+
+  def get_all(self, table):
+    with sql.connect("database.db") as con:
+      cur = con.cursor()
+      cur.execute("SELECT * from %s" % table)
+      return cur.fetchall()
+
+
+  def search(self, table,  column, data):
+    with sql.connect(self.database) as con:
+      cur = con.cursor()
+      cur.execute("SELECT * from %s WHERE %s LIKE '%s'" % (table, column, data))
+      records = cur.fetchall()
+      return records
+  
+  def delete(self, table, column, data):
+    if self.exists( table, column, data):
+      with sql.connect(self.database) as con:
+        cur = con.cursor()
+        cur.execute("DELETE from %s WHERE %s LIKE '%s'" % (table, column, data))
+        return True
+
+  def exists(self, table, column, data):
+    with sql.connect(self.database) as con:
+      cur = con.cursor()
+      cur.execute("SELECT * FROM %s WHERE (EXISTS(SELECT 1 FROM %s WHERE %s='%s'))" % (table,table, column, data))
+      if cur.fetchone():
+        return True
+      else:
+        return False
+  
+  def insert(self, table, table_columns, data):
+    with sql.connect(self.database) as con:
+      cur = con.cursor()
+      cur.execute("INSERT INTO %s (%s, %s, %s) VALUES ('%s', '%s', '%s')" % (table, table_columns[0], table_columns[1], table_columns[2], data.user, data.passwd, data.admin))
+      con.commit()
+      return True
